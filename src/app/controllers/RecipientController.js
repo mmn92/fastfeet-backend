9ab33742ede8b062aa48fc473025fbcc3e687df3
mmn92 <matheus.mota.nogueira@gmail.com>
2020-02-19 20:51:54 -0300

    create recipient model and controller

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,93 @@
+import * as Yup from 'yup';
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async index(req, res) {
+    const recipients = await Recipient.findAll();
+
+    return res.json(recipients);
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+    const findRecipient = await Recipient.findOne({ where: { id } });
+
+    if (!findRecipient) {
+      return res.status(401).json({ error: 'Recipient not found' });
+    }
+
+    return res.json(findRecipient);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      postal: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation error' });
+    }
+
+    const findRecipient = await Recipient.findOne({ where: req.body });
+
+    if (findRecipient) {
+      return res.status(400).json({ error: 'Recipient already registered' });
+    }
+
+    const recipient = await Recipient.create(req.body);
+
+    return res.json(recipient);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.string(),
+      complement: Yup.string(),
+      state: Yup.string(),
+      city: Yup.string(),
+      postal: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation error' });
+    }
+
+    const { id } = req.params;
+
+    const recipient = await Recipient.findByPk(id);
+
+    if (!recipient) {
+      return res.status(400).json({ error: 'Recipent not found' });
+    }
+
+    const {
+      name,
+      stree,
+      number,
+      complement,
+      state,
+      city,
+      postal,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      name,
+      stree,
+      number,
+      complement,
+      state,
+      city,
+      postal,
+    });
+  }
+}
+
+export default new RecipientController();
