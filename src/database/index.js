44e02641dc954653c028277de7ecf0a612e86a97
mmn92 <matheus.mota.nogueira@gmail.com>
2020-02-20 00:09:45 -0300

    configure associations and create new routes

--- src/database/index.js
+++ src/database/index.js
@@ -4,8 +4,11 @@ import dbConfig from '../config/database';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
+import File from '../app/models/File';
+import Courier from '../app/models/Courier';
+import Delivery from '../app/models/Delivery';
 
-const models = [User, Recipient];
+const models = [User, Recipient, File, Courier, Delivery];
 
 class Database {
   constructor() {
@@ -15,7 +18,9 @@ class Database {
   init() {
     this.connection = new Sequelize(dbConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import multerconfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
+import CourierController from './app/controllers/CourierController';
+import DeliveryController from './app/controllers/DeliveryController';
 
 const routes = Router();
-
-routes.get('/', (req, res) => {
-  res.send({ ok: true });
-});
+const upload = multer(multerconfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -23,5 +25,15 @@ routes.put('/recipients/:id', RecipientController.update);
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+
+routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/couriers', CourierController.index);
+routes.post('/couriers', CourierController.store);
+routes.put('/couriers/:id', CourierController.update);
+routes.delete('/couriers/:id', CourierController.delete);
+
+routes.get('/deliveries', DeliveryController.index);
+routes.post('/deliveries', DeliveryController.store);
 
 export default routes;
